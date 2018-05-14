b8fa7643cd681ba3610781030b713a8cae215c70
tiennguyen98 <nguyentien1098@gmail.com>
2018-05-14 23:19:34 +0700

    OK

---
+++ js/control.js
@@ -0,0 +1,165 @@
+function getValues(){
+	var cells = document.getElementsByClassName('cell');
+	var collect = [];
+	var current = 0;
+	for(var i = 0; i < 9; i++){
+		var row = [];
+		for(var j = 0; j < 9; j++){
+			let x = current++;
+			if (cells[x].value != '' && cells[x].value != NaN) {
+				row.push(parseInt(cells[x].value));
+			} else {
+				row.push(UNASSIGNED);
+			}
+		}
+		collect.push(row);
+	}
+	return collect;
+}
+
+// function fillToInputs(puzzle, inputs, time){
+// 	var cell = 0;
+// 	for(var i = 0; i < puzzle.length; i++){
+// 		for(var j = 0; j < puzzle[i].length; j++){
+// 			inputs[cell++].value = puzzle[i][j];
+// 		}
+// 	}
+// }
+
+function fillToInputs(puzzle, inputs, time = 0){
+	if (time == 0) {
+		var cell = 0;
+		for(var i = 0; i < puzzle.length; i++){
+			for(var j = 0; j < puzzle[i].length; j++){
+				inputs[cell++].value = puzzle[i][j];
+			}
+		}
+	} else {
+		$('.cell').attr('disabled', '');
+		var merge = [];
+		var count = 0;
+		for(var i = 0; i < puzzle.length; i++){
+			for(var j = 0; j < puzzle[i].length; j++){
+				merge[count++] = puzzle[i][j];
+			}
+		}
+		var cell = 0;
+		var sleep = setInterval(function(){
+			var number = cell++;
+			if (inputs[number].value == UNASSIGNED) {
+				inputs[number].value = merge[number];
+			} 
+			if (cell >= 81) {
+				clearInterval(sleep);
+				$('.cell').removeAttr('disabled', '');
+			}
+		}, time * 1000);
+	}
+}
+
+
+function setNewGame(level = 1) {
+	level = parseInt(level);
+	if (level == 1)
+		cellCount = 50;
+	else if (level == 2)
+		cellCount = 35;
+	else if (level == 3)
+		cellCount = 25;
+	else 
+		return false;
+	while (true) {
+		let random  = randomPuzzle();
+		if (solveSudoku(random, 0, 0)) {
+			valid = random;
+			break;
+		}
+	}
+	var ge = generatePuzzle(cellCount);
+	fillToInputs(ge, inputs);
+}
+
+function displaySolution(time){
+	if (valid.length < 9) {
+		$('.alert').each(function(){
+			$(this).removeClass('alert-success');
+			$(this).addClass('alert-danger');
+			$(this).show();
+			$(this).html('Bạn chưa tạo game mới!');
+		});
+		return false;
+	}
+	var solve = solveSudoku(valid, 0, 0);
+	if (!solve) {
+		$('.alert').each(function(){
+			$(this).removeClass('alert-success');
+			$(this).addClass('alert-danger');
+			$(this).show();
+			$(this).html('Game lỗi!');
+		});
+		return false;
+	}
+	fillToInputs(solve, inputs, time);
+}
+
+function reset() {
+	$('.alert').hide();
+	valid = [];
+	$('.cell').css({'background': '#fff'});
+	fillToInputs(generateEmptyPuzzle(), inputs);
+}
+
+function clickNewGame() {
+	let level = document.getElementById('level_number').value;
+	setNewGame(level);
+	window.alert('Tạo thành công!!!');
+}
+
+function check(){
+	$('.cell').each(function(){
+		if ($(this).val() == "" || $(this).val() == 0) {
+			$('.alert').each(function(){
+				$(this).removeClass('alert-success');
+				$(this).addClass('alert-danger');
+				$(this).show();
+				$(this).html('Bạn chưa hoàn thành!');
+
+			});
+			return false;
+		}
+	});
+	var checkSolve = isSovled(getValues());
+	if (checkSolve === true) {
+		$('.alert').removeClass('alert-danger');
+		$('.alert').addClass('alert-success');
+		$('.alert').show();
+		$('.alert').html('Bạn đã giải thành công!');
+	} else{
+		$('.alert').removeClass('alert-success');
+		$('.alert').addClass('alert-danger');
+		$('.alert').show();
+		$('.alert').html('Bạn giải chưa đúng!');
+	}
+}
+
+function shuffle(b) {
+	let stringZeroToNine = JSON.stringify(b);
+	let a = JSON.parse(stringZeroToNine);
+	var j, x, i;
+	for (i = a.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+	return a;
+}
+
+// Check valid number
+$(document).ready(function(){
+	$('table input[type="number"]').keyup(function(){
+		if ($(this).val() < 0 || $(this).val() > 9) {
+			$(this).val(0);
+		}
+	});
+});
---
+++ js/sudoku.js
@@ -0,0 +1,158 @@
+const N = 9;
+const UNASSIGNED = "";
+
+const inputs = document.getElementsByClassName('cell');
+const alert = document.getElementById('alert');
+var valid = [];
+function usedInRow(grid, row, num) {
+    return grid[row].indexOf(num) == -1 ? false : true;
+}
+
+function usedInColumn(grid, col, num) {
+	var column = []; //Lấy ra hàng 
+	for(var i = 0; i < N; i++){
+		column.push(grid[i][col]); // Gộp hàng và cột vào 1 mảng
+	}
+    return column.indexOf(num) == -1 ? false : true;
+}
+
+function usedInBox(grid, boxStartRow, boxStartCol, num) {
+    for (row = 0; row < 3; row++)
+    for (col = 0; col < 3; col++)
+        if (grid[row + boxStartRow][col + boxStartCol] == num)
+        return true;
+    return false;
+}
+
+function isSafe(grid, row, col, num) {
+    return !usedInRow(grid, row, num) &&
+        !usedInColumn(grid, col, num) &&
+        !usedInBox(grid, row - row % 3, col - col % 3, num);
+}
+
+
+function conrrectElementInRow(grid, row, num) {
+    return grid[row].indexOf(num) != grid[row].lastIndexOf(num) ? false : true;
+}
+
+function conrrectElementInColumn(grid, col, num) {
+	var column = []; //Lấy ra hàng 
+	for(var i = 0; i < N; i++){
+		column.push(grid[i][col]); // Gộp hàng và cột vào 1 mảng
+	}
+    return column.indexOf(num) != column.lastIndexOf(num) ? false : true;
+}
+
+function conrrectElementInBox(grid, rowIndex, columnIndex, num) {
+    if (rowIndex % 3 == 0)
+		startRow = rowIndex; // Nếu chia hết cho 3 thì chính là nó
+	else
+		startRow = rowIndex - rowIndex % 3; // Nếu không tìm vị trí bắt đầu
+
+	// Tìm vị trí bắt đầu column của box. Tương tự
+	if (columnIndex % 3 == 0)
+		startColumn = columnIndex;
+	else
+		startColumn = columnIndex - columnIndex % 3;
+
+	var firstRow = grid[startRow].slice(startColumn, startColumn + 3);
+	var secondRow = grid[startRow + 1].slice(startColumn, startColumn + 3);
+	var thirdRow = grid[startRow + 2].slice(startColumn, startColumn + 3);
+	var boxGrid = firstRow.concat(secondRow, thirdRow);
+	return boxGrid.indexOf(num) != boxGrid.lastIndexOf(num) ? false : true;
+}
+
+function correct(grid, row, col, num){
+	return conrrectElementInRow(grid, row, num) && conrrectElementInColumn(grid, col, num) && conrrectElementInBox(grid, row, col, num);
+}
+
+function findUnassignedLocation(grid, row, col) {
+    for (row = 0; row < N; row++)
+    for (col = 0; col < N; col++)
+        if (grid[row][col] == UNASSIGNED)
+        return [row, col];
+    return false;
+}
+
+function solveSudoku(grid, row, col) {
+    row = col = 0;
+    var unAssignedLocation = findUnassignedLocation(grid, row, col);
+    if (!unAssignedLocation)
+    	return grid; 
+    row = unAssignedLocation[0];
+    col = unAssignedLocation[1];
+    for (var num = 1; num <= N; num++) {
+	    if (isSafe(grid, row, col, num)) {
+	        grid[row][col] = num; 
+	        if (solveSudoku(grid, row, col))
+	        return grid; 
+	        grid[row][col] = UNASSIGNED;  
+	    }
+    }
+    return false; 
+}
+
+
+function randomPuzzle(){
+	var empty = this.generateEmptyPuzzle();
+	var firstRow = shuffle([...Array(10).keys()]);
+	firstRow.shift();
+	empty[0] = firstRow;
+	for (var x = 1; x < 9; x++) {
+		var validRow = [];
+		for(var i = 0; i < 9; i++){
+			let ba = Math.floor(Math.random() * 8) + 0;
+			if (validRow.indexOf(ba) == -1 && isSafe(empty, x, i, ba)) 
+				validRow.push(ba);
+			else
+				validRow.push(UNASSIGNED);
+			if (validRow.length >= 9) {
+				break;
+			}
+		}
+		empty[x] = validRow;
+	}
+	return empty;
+}
+
+
+function generatePuzzle(cellCount){
+	if (Number.isInteger(cellCount) && cellCount >= 0 && cellCount < 81) {
+		if (cellCount === 0) {
+			return this.generateEmptyPuzzle();
+		} else {
+			var cells = Array.apply(null, 
+							Array(cellCount)).map(function() { 
+								return Math.floor(Math.random() * 79) + 0; 
+						});
+			var i = 0;
+			for(var row = 0; row < 9; row++){
+				for(var column = 0; column < 9; column++){
+					if (cells.indexOf(i++) == -1) {
+						valid[row][column] = UNASSIGNED ;
+					}
+				}
+			}
+			return valid;
+		}
+	}
+};
+function generateEmptyPuzzle(isReset = true){
+	var elm = isReset == true ? '' : 0;
+	return new Array(9).fill(new Array(9).fill(elm));
+};
+
+function isSovled(puzzle){
+	var cell = 0;
+	for(var i = 0; i < 9; i++){
+		for(var j = 0; j < 9; j++){
+			var num = puzzle[i][j];
+			var c = cell++;
+			if (!correct(puzzle, i, j, num) || findUnassignedLocation(puzzle, i, j) != false) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
